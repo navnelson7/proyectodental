8f590af83994bc1a990d752802b3d56794e58dd1
Nelson-developer <nelsonher019@gmail.com>
2020-11-19 21:38:41 -0600

    React router dom configurado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Fragment } from 'react'
 import NavbarDesktop from './components/Navbar/NavbarDesktop'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return (
     <Fragment>
-      <NavbarDesktop />
-    </Fragment>
+      <Router>
+        <NavbarDesktop />
+        <Switch>
+          <Route path="/">
+           <Home/>
+          </Route>
+        </Switch>
+    </Router>
+    </Fragment >
   )
 }
 
---
+++ src/components/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <p align="center">Hello!</p>
+    )
+}
+
+export default Home
